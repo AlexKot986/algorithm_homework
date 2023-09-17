29ea44400e9dd9d88eef394fe96e31ec807fb818
Alexershov86 <alexershov86@mail.ru>
2023-09-17 19:25:11 +0300

    добавлена документация

--- home_work.py
+++ home_work.py
@@ -19,12 +19,16 @@ class Node:
 
 
 class BinaryTree:
-
+    """Конструктор создания дерева, где root_value значение ноды root"""
     def __init__(self, root_value):
         self.root = Node(root_value)
+        """size - количество элементов в дереве"""
         self.size = 1
 
     def add(self, value):
+        """Добавление нового элемента
+        ищем ноду по значению value, если такой ноды нет то добывляем новую ноду со значением value
+        к родителю (res[1]) и увеличиваем значение size на 1"""
         res = self.search(self.root, value)
 
         if res[0] is None:
@@ -40,6 +44,8 @@ class BinaryTree:
 
 
     def search(self, node, value, parent=None):
+        """Поиск ноды по значению от ноды (node), где parent - это родитель ноды (node)
+        если нода со значением value найдена или следующей ноды нет, то возвращаем ноду и ее родителя"""
         if node == None or value == node.value:
             return node, parent
         if value > node.value:
@@ -49,7 +55,11 @@ class BinaryTree:
         
 
     def __listValue(self, node, listValue):
+        """метод служит для записи значений (value) от текущей ноды (node) в список (listValue)
+        через обход дерева в глубину рекурсивно"""
         if node.left != None:
+            """пока слудующая нода существует то добавляем ее значение в список и
+            рекурсивно вызываем метод __listValue для следующей ноды"""
             listValue.append(node.left.value)
             self.__listValue(node.left, listValue)
         if node.right != None:
@@ -58,26 +68,33 @@ class BinaryTree:
 
 
     def delValue(self, value):
+        """Удаление элемента.
+        Ищем ноду по значению если она существует или выводим сообщение что такого значения нет"""
         res = self.search(self.root, value)
         if res[0] != None:    
-
+            """добавляем все элементы дерева от найденной ноды в список и отнимаем значение размера
+            списка от текущего размера (size)"""
             if res[0].value == value:
                 listValue = []
                 self.__listValue(res[0], listValue)
                 self.size -= len(listValue) + 1
-
+                """если найденный элемент является нодой (root), то сортируем список, создаем
+                новое дерево и в качестве значения ноды (root) берем средний элемент списка (listValue)"""
                 if value == self.root.value:
                     listValue.sort()
                     self.__init__(listValue[len(listValue) // 2])
-                    
+                    """дабавляем элементы в дерево из списка, исключая средний элемент"""
                     for i in range(len(listValue)):
                         if i != len(listValue) // 2:
                             self.add(listValue[i])         
                 else:
+                    """есле значение элемента больше значения родителя (res[1]), то удаляем правую
+                    ветку от родителя, если меньше то удаляем левую ветку"""
                     if value > res[1].value:
                         res[1].right = None
                     if value < res[1].value:
                         res[1].left = None
+                    """добавляем элементы из списка (listValue) в дерево"""
                     for i in listValue:
                         self.add(i)
         else:
@@ -85,6 +102,7 @@ class BinaryTree:
         
 
     def printTree(self):
+        """добавляем все элементы дерева в список, сортируем и выводим на консоль"""
         listValue = []
         self.__listValue(self.root, listValue)
         listValue.append(self.root.value)
